10bac87dc54bb5292a7f086a21ec0903a905930a
Russell Standish <hpcoder@hpcoders.com.au>
2025-09-29 10:59:59 +1000

    Downgrade fontconfig to a version that will compile on OpenSUSE.

--- src/fontconfig.mk
+++ src/fontconfig.mk
@@ -3,8 +3,8 @@
 PKG             := fontconfig
 $(PKG)_WEBSITE  := https://fontconfig.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.16.0
-$(PKG)_CHECKSUM := 6a33dc555cc9ba8b10caf7695878ef134eeb36d0af366041f639b1da9b6ed220
+$(PKG)_VERSION  := 2.14.0
+$(PKG)_CHECKSUM := dcbeb84c9c74bbfdb133d535fe1c7bedc9f2221a8daf3914b984c44c520e9bac
 $(PKG)_SUBDIR   := fontconfig-$($(PKG)_VERSION)
 $(PKG)_FILE     := fontconfig-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://fontconfig.org/release/$($(PKG)_FILE)
